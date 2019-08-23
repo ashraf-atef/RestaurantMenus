729912f8a4f37b908223e91a24db18b4f0bb7cc7
Ashraf-atef <ashrafatef84@gmail.com>
2019-08-23 19:19:03 +0200

    Convert endless recyclerview listener to koltin

---
+++ app/src/main/java/com/example/restaurant/common/presentationLayer/EndlessRecyclerViewOnScrollListener.kt
@@ -0,0 +1,50 @@
+package com.example.restaurant.common.presentationLayer
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class EndlessRecyclerViewOnScrollListener : RecyclerView.OnScrollListener() {
+    /**
+     * The total number of items in the data after the last load
+     */
+    private var previousTotal = 0
+    /**
+     * True if we are still waiting for the last set of data to load.
+     */
+    private var isLoading = true
+
+    private val loadMoreRunnable = Runnable { this.onLoadMore() }
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+        if (dx > 0) { // scrolling end (right / left depend on locale)
+            val visibleItemCount = recyclerView.childCount
+            val totalItemCount = recyclerView.layoutManager!!.itemCount
+            val firstVisibleItem = (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+
+            if (isLoading) {
+                if (totalItemCount > previousTotal + 1) { // the + 1 is for the error / progress layouts
+                    isLoading = false
+                    previousTotal = totalItemCount
+                }
+            }
+            val visibleThreshold = 5
+            if (!isLoading && totalItemCount - visibleItemCount <= firstVisibleItem + visibleThreshold) {
+                // End has been reached
+
+                recyclerView.post(loadMoreRunnable)
+
+                isLoading = true
+            }
+        }
+    }
+
+
+    // Defines the process for actually loading more data based on page
+    abstract fun onLoadMore()
+
+    fun restState() {
+        previousTotal = 0
+        isLoading = true
+    }
+}
